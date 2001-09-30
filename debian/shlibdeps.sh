acf6221d1307925dd6c99004bb690de649317d7f
Siggi Langauf <siggi@users.sourceforge.net>
2001-09-30 19:00:20 +0000

    relaxed library dependancies
    CVS patchset: 709
    CVS date: 2001/09/30 19:00:20

---
+++ debian/shlibdeps.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# shlibdeps.sh - script to calculate depends/recommends/suggests for shlibs
+#
+# usage: debian/shlibdeps.sh <packagename>
+#
+# (C) 2001 Siggi Langauf <siggi@debian.org>
+
+installdir=debian/$1
+
+OPTIONAL="$installdir/usr/lib/xine/plugins/xineplug_ao_out_alsa05.so
+	  $installdir/usr/lib/xine/plugins/xineplug_ao_out_arts.so
+	  $installdir/usr/lib/xine/plugins/xineplug_ao_out_esd.so
+	  $installdir/usr/lib/xine/plugins/xineplug_ao_out_oss.so
+	  $installdir/usr/lib/xine/plugins/xineplug_vo_out_aa.so
+	  $installdir/usr/lib/xine/plugins/xineplug_vo_out_syncfb.so
+	  $installdir/usr/lib/xine/plugins/xineplug_vo_out_xv.so"
+
+RECOMMENDED="$installdir/usr/lib/xine/plugins/xineplug_vo_out_aa.so"
+
+#start with all executables and shared objects
+REQUIRED=`find $installdir -type f \( -name \*.so -o -perm +111 \)`
+
+
+#remove all OPTIONAL or RECOMMENDED stuff
+for file in `echo $OPTIONAL $RECOMMENDED`; do
+    REQUIRED=`echo "$REQUIRED" | grep -v $file`
+done
+
+dpkg-shlibdeps -Tdebian/$1.substvars \
+               $REQUIRED -dRecommends $RECOMMENDED -dSuggests $OPTIONAL
