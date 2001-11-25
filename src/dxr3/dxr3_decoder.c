c06b85653084614aa4e0a58fb095823c5fb5edb5
Harm van der Heijden <hrm@users.sourceforge.net>
2001-11-25 21:13:15 +0000

    - accidentally commited leftover from failed experiment last time; now removed.
    CVS patchset: 1111
    CVS date: 2001/11/25 21:13:15

--- src/dxr3/dxr3_decoder.c
+++ src/dxr3/dxr3_decoder.c
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
  *
- * $Id: dxr3_decoder.c,v 1.39 2001/11/25 20:21:25 hrm Exp $
+ * $Id: dxr3_decoder.c,v 1.40 2001/11/25 21:13:15 hrm Exp $
  *
  * dxr3 video and spu decoder plugin. Accepts the video and spu data
  * from XINE and sends it directly to the corresponding dxr3 devices.
@@ -485,8 +485,6 @@ static void dxr3_decode_data (video_decoder_t *this_gen, buf_element_t *buf)
 		/* store at what time we called draw last */
 		this->last_scr = img->SCR;
 		img->free(img);
-		vpts = this->video_decoder.metronom->got_video_frame(
-		 this->video_decoder.metronom, buf->PTS, buf->SCR );
 	}
 
 	/* for stills we're done now */
