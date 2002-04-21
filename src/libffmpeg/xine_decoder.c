533caec4d7c0d4ca2e831ba414bffc52c0f0cf46
Miguel Freitas <miguelfreitas@users.sourceforge.net>
2002-04-21 02:56:39 +0000

    really handle reported types
    CVS patchset: 1752
    CVS date: 2002/04/21 02:56:39

--- src/libffmpeg/xine_decoder.c
+++ src/libffmpeg/xine_decoder.c
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
  *
- * $Id: xine_decoder.c,v 1.31 2002/04/20 20:20:43 guenter Exp $
+ * $Id: xine_decoder.c,v 1.32 2002/04/21 02:56:39 miguelfreitas Exp $
  *
  * xine decoder plugin using ffmpeg
  *
@@ -176,6 +176,8 @@ static void ff_decode_data (video_decoder_t *this_gen, buf_element_t *buf) {
       codec = avcodec_find_decoder (CODEC_ID_MSMPEG4);
       break;
     case BUF_VIDEO_MPEG4 :
+    case BUF_VIDEO_XVID :
+    case BUF_VIDEO_DIVX5 :
       codec = avcodec_find_decoder (CODEC_ID_MPEG4);
       break;
     case BUF_VIDEO_JPEG:
