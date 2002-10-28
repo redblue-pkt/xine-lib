7dd479235e47a38bc1944bf3ba483aecdf75855e
Mike Melanson <mike@multimedia.cx>
2002-10-28 05:26:35 +0000

    bump the interface version
    CVS patchset: 3060
    CVS date: 2002/10/28 05:26:35

--- src/demuxers/demux_mpeg.c
+++ src/demuxers/demux_mpeg.c
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
  *
- * $Id: demux_mpeg.c,v 1.92 2002/10/28 03:24:43 miguelfreitas Exp $
+ * $Id: demux_mpeg.c,v 1.93 2002/10/28 05:26:35 tmmm Exp $
  *
  * demultiplexer for mpeg 1/2 program streams
  * reads streams of variable blocksizes
@@ -1125,6 +1125,6 @@ static void *init_plugin (xine_t *xine, void *data) {
 
 plugin_info_t xine_plugin_info[] = {
   /* type, API, "name", version, special_info, init_function */  
-  { PLUGIN_DEMUX, 14, "mpeg", XINE_VERSION_CODE, NULL, init_plugin },
+  { PLUGIN_DEMUX, 15, "mpeg", XINE_VERSION_CODE, NULL, init_plugin },
   { PLUGIN_NONE, 0, "", 0, NULL, NULL }
 };
