e94b243c36c52ec673231f975eacedceb01f616c
Miguel Freitas <miguelfreitas@users.sourceforge.net>
2002-12-16 18:58:53 +0000

    update libfaad
    CVS patchset: 3559
    CVS date: 2002/12/16 18:58:53

--- src/libfaad/codebook/hcb_2.c
+++ src/libfaad/codebook/hcb_2.c
@@ -16,7 +16,7 @@
 ** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 **
-** $Id: hcb_2.c,v 1.1 2002/07/14 23:33:35 miguelfreitas Exp $
+** $Id: hcb_2.c,v 1.2 2002/12/16 18:58:57 miguelfreitas Exp $
 **/
 
 #include "../common.h"
