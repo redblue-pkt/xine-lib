66aaf6e81803f94993475e8dce9d56873b17664b
Guenter Bartsch <guenter@users.sourceforge.net>
2003-01-05 13:11:53 +0000

    fixing a copyright, bringing back the old READMEs to the xine-lib tarball, improve makefiles so hopefully documentation is installed in the right place, add text version of the faq
    CVS patchset: 3783
    CVS date: 2003/01/05 13:11:53

--- src/xine-engine/video_out.c
+++ src/xine-engine/video_out.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2000-2002 the xine project
+ * Copyright (C) 2000-2003 the xine project
  * 
  * This file is part of xine, a free video player.
  * 
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
  *
- * $Id: video_out.c,v 1.127 2002/12/27 19:14:41 mroi Exp $
+ * $Id: video_out.c,v 1.128 2003/01/05 13:11:55 guenter Exp $
  *
  * frame allocation / queuing / scheduling / output functions
  */
