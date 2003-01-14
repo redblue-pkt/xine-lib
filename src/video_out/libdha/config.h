50a43b20f9f2797d264564001e638d6f9ba64890
Miguel Freitas <miguelfreitas@users.sourceforge.net>
2003-01-14 01:56:59 +0000

    ops, missing files
    CVS patchset: 3910
    CVS date: 2003/01/14 01:56:59

---
+++ src/video_out/libdha/config.h
@@ -0,0 +1,10 @@
+#ifndef LIBDHA_CONFIG_H
+#define LIBDHA_CONFIG_H
+
+#include "../config.h"
+
+#ifdef TARGET_LINUX
+//#define CONFIG_DHAHELPER /* doesn't affect virt_to_phys */
+#endif
+
+#endif /* LIBDHA_CONFIG_H */
