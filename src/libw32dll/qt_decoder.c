afc545b71564eb83804ddc0726607f5d55fba8cf
Miguel Freitas <miguelfreitas@users.sourceforge.net>
2003-01-29 17:51:05 +0000

    fix hang problem reported by bastien
    CVS patchset: 4042
    CVS date: 2003/01/29 17:51:05

--- src/libw32dll/qt_decoder.c
+++ src/libw32dll/qt_decoder.c
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
  *
- * $Id: qt_decoder.c,v 1.12 2003/01/29 17:16:01 miguelfreitas Exp $
+ * $Id: qt_decoder.c,v 1.13 2003/01/29 17:51:05 miguelfreitas Exp $
  *
  * quicktime video/audio decoder plugin, using win32 dlls
  * most of this code comes directly from MPlayer
@@ -240,6 +240,7 @@ static void qta_init_driver (qta_decoder_t *this, buf_element_t *buf) {
   
   if (this->qtml_dll == NULL ) {
     printf ("qt_audio: failed to load dll\n" );
+    pthread_mutex_unlock(&win32_codec_mutex);
     return;
   }
 
@@ -795,6 +796,7 @@ static void qtv_init_driver (qtv_decoder_t *this, buf_element_t *buf) {
   
   if (this->qtml_dll == NULL ) {
     printf ("qt_video: failed to load dll\n" );
+    pthread_mutex_unlock(&win32_codec_mutex);
     return;
   }
 
