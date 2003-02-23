66350bf2556815f620b376de21d410a598cd4df9
Thibaut Mattern <tmattern@users.sourceforge.net>
2003-02-23 15:56:42 +0000

    Wow, I've forgotten a big part of my patch !
    CVS patchset: 4264
    CVS date: 2003/02/23 15:56:42

--- src/input/net_buf_ctrl.c
+++ src/input/net_buf_ctrl.c
@@ -1,18 +1,18 @@
 /*
  * Copyright (C) 2000-2002 the xine project
- * 
+ *
  * This file is part of xine, a free video player.
- * 
+ *
  * xine is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * xine is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
@@ -32,8 +32,8 @@
 
 #define DEFAULT_LOW_WATER_MARK     1
 #define DEFAULT_HIGH_WATER_MARK 5000 /* in millisecond */
-#define VIDEO_FIFO_BUFS          499 /* 500 - 1 */
-#define AUDIO_FIFO_BUFS          229 /* 230 - 1 */
+#define VIDEO_FIFO_BUFS          498 /* 500 - 2 */
+#define AUDIO_FIFO_BUFS          228 /* 230 - 2 */
 
 /*
 #define LOG
@@ -198,9 +198,13 @@ nbc_t *nbc_init (xine_stream_t *stream) {
 
 
 void nbc_set_high_water_mark(nbc_t *this, int value) {
+/*
   this->high_water_mark = value;
+*/
 }
 
 void nbc_set_low_water_mark(nbc_t *this, int value) {
+/*
   this->low_water_mark = value;
+*/
 }
