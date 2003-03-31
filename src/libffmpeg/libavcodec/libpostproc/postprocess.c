78f5ec7b1d7287d3a3df25d36f71e3f9bf9a8b29
Daniel Caujolle-Bert <f1rmb@users.sourceforge.net>
2003-03-31 08:09:04 +0000

    including malloc.h on some system is depreciated
    CVS patchset: 4517
    CVS date: 2003/03/31 08:09:04

--- src/libffmpeg/libavcodec/libpostproc/postprocess.c
+++ src/libffmpeg/libavcodec/libpostproc/postprocess.c
@@ -73,9 +73,10 @@ try to unroll inner for(x=0 ... loop to avoid these damn if(x ... checks
 
 #include "xineutils.h"
 
-#ifdef HAVE_MALLOC_H
-#include <malloc.h>
-#endif
+#include <stdlib.h>
+//#ifdef HAVE_MALLOC_H
+//#include <malloc.h>
+//#endif
 //#undef HAVE_MMX2
 //#define HAVE_3DNOW
 //#undef HAVE_MMX
