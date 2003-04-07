6686c951f45e97c06ba05ab74d7ab2d83ec24ad7
James Courtier-Dutton <jcdutton@users.sourceforge.net>
2003-04-07 19:59:09 +0000

    Get xine cvs to compile with xine's version of libdvdnav.
    CVS patchset: 4568
    CVS date: 2003/04/07 19:59:09

--- src/input/libdvdnav/dvdnav.h
+++ src/input/libdvdnav/dvdnav.h
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
  *
- * $Id: dvdnav.h,v 1.9 2003/04/07 18:10:48 mroi Exp $
+ * $Id: dvdnav.h,v 1.10 2003/04/07 19:59:09 jcdutton Exp $
  *
  */
 
@@ -33,12 +33,12 @@
 extern "C" {
 #endif
 
-#ifdef DVDNAV_COMPILE
-#  include "dvdnav_events.h"
-#  include "dvd_types.h"
-#else
+#ifdef HAVE_DVDNAV
 #  include <dvdnav/dvdnav_events.h>
 #  include <dvdnav/dvd_types.h>
+#else
+#  include "dvdnav_events.h"
+#  include "dvd_types.h"
 #endif
 
 #include "dvd_reader.h"
