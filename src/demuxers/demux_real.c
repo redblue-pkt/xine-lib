9bba47110d6fb163934012317a44304090c69dee
Guenter Bartsch <guenter@users.sourceforge.net>
2003-04-09 09:57:29 +0000

    another real mimetype (this time from nautilus)
    CVS patchset: 4576
    CVS date: 2003/04/09 09:57:29

--- src/demuxers/demux_real.c
+++ src/demuxers/demux_real.c
@@ -28,7 +28,7 @@
  *   
  *   Based on FFmpeg's libav/rm.c.
  *
- * $Id: demux_real.c,v 1.46 2003/03/28 14:56:02 miguelfreitas Exp $
+ * $Id: demux_real.c,v 1.47 2003/04/09 09:57:29 guenter Exp $
  */
 
 #ifdef HAVE_CONFIG_H
@@ -1269,7 +1269,8 @@ static char *get_extensions (demux_class_t *this_gen) {
 
 static char *get_mimetypes (demux_class_t *this_gen) {
   return "audio/x-pn-realaudio: ra, rm, ram: Real Media file;"
-         "audio/x-pn-realaudio-plugin: rpm: Real Media plugin file;"; 
+         "audio/x-pn-realaudio-plugin: rpm: Real Media plugin file;"
+         "audio/x-real-audio: ra, rm, ram: Real Media file;"; 
 }
 
 static void class_dispose (demux_class_t *this_gen) {
