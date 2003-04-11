5f17dc0c56384be554747416e668f5f6fcbe8057
Miguel Freitas <miguelfreitas@users.sourceforge.net>
2003-04-11 02:04:29 +0000

    fix long standing fastforward bug. symptom: video fifo get empty, playback gets sluggish, lot of cpu unused. bug 687526
    CVS patchset: 4586
    CVS date: 2003/04/11 02:04:29

--- src/xine-engine/audio_out.c
+++ src/xine-engine/audio_out.c
@@ -17,7 +17,7 @@
  * along with self program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
  *
- * $Id: audio_out.c,v 1.116 2003/04/02 12:28:08 hadess Exp $
+ * $Id: audio_out.c,v 1.117 2003/04/11 02:04:29 miguelfreitas Exp $
  * 
  * 22-8-2001 James imported some useful AC3 sections from the previous alsa driver.
  *   (c) 2001 Andy Lo A Foe <andy@alsaplayer.org>
@@ -766,6 +766,7 @@ static void *ao_loop (void *this_gen) {
 #endif
 	  fifo_append (this->free_fifo, in_buf);
 	  in_buf = NULL;
+	  continue;
 	}
       }
 
