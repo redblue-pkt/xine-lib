6298ee36126ceae6f0437b91ba9a74998af3dd33
Miguel Freitas <miguelfreitas@users.sourceforge.net>
2003-04-13 22:19:08 +0000

    make compressor disabled by default. it was burning cpu cicles after setting AMP level. played a xvid+ac3 stream measuring function time with oprofile: audio_filter_amp_compress 64.63% (relative to total libxine.so time) for comparison: xine_fast_memcpy 13.27%
    CVS patchset: 4609
    CVS date: 2003/04/13 22:19:08

--- src/xine-engine/audio_out.c
+++ src/xine-engine/audio_out.c
@@ -17,7 +17,7 @@
  * along with self program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
  *
- * $Id: audio_out.c,v 1.117 2003/04/11 02:04:29 miguelfreitas Exp $
+ * $Id: audio_out.c,v 1.118 2003/04/13 22:19:08 miguelfreitas Exp $
  * 
  * 22-8-2001 James imported some useful AC3 sections from the previous alsa driver.
  *   (c) 2001 Andy Lo A Foe <andy@alsaplayer.org>
@@ -1603,7 +1603,7 @@ xine_audio_port_t *ao_new_port (xine_t *xine, ao_driver_t *driver,
 						   NULL, 10, NULL, NULL);
 
   this->compression_factor     = 1.0;
-  this->compression_factor_max = 4.0;
+  this->compression_factor_max = 0.0;
   this->amp_factor             = 1.0;
   this->do_compress            = 0;
 
