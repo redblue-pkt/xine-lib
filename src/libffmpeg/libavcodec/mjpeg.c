ddba2c8304bdce3b71186a591e4c3980c89be791
James Stembridge <jstembridge@users.sourceforge.net>
2003-10-16 22:03:36 +0000

    Don't read off the end of the buffer
    CVS patchset: 5531
    CVS date: 2003/10/16 22:03:36

--- src/libffmpeg/libavcodec/mjpeg.c
+++ src/libffmpeg/libavcodec/mjpeg.c
@@ -1667,7 +1667,7 @@ static int find_marker(uint8_t **pbuf_ptr, uint8_t *buf_end)
     while (buf_ptr < buf_end) {
         v = *buf_ptr++;
 	v2 = *buf_ptr;
-        if ((v == 0xff) && (v2 >= 0xc0) && (v2 <= 0xfe)) {
+        if ((buf_ptr < buf_end) && (v == 0xff) && (v2 >= 0xc0) && (v2 <= 0xfe)) {
 	    val = *buf_ptr++;
 	    goto found;
         }
