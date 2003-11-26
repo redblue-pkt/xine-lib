f3370d39602f871ffbf01e694e9a035985d88060
Michael Roitzsch <mroi@users.sourceforge.net>
2003-11-26 19:36:13 +0000

    adding the usual header
    CVS patchset: 5795
    CVS date: 2003/11/26 19:36:13

--- src/video_out/x11osd.h
+++ src/video_out/x11osd.h
@@ -1,3 +1,32 @@
+/* 
+ * Copyright (C) 2003 the xine project
+ *
+ * This file is part of xine, a free video player.
+ *
+ * xine is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * xine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ *
+ * $Id: x11osd.h,v 1.2 2003/11/26 19:36:13 mroi Exp $
+ *
+ * x11osd.h, use X11 Nonrectangular Window Shape Extension to draw xine OSD
+ *
+ * Nov 2003 - Miguel Freitas
+ *
+ * based on ideas and code of
+ * xosd Copyright (c) 2000 Andre Renaud (andre@ignavus.net)
+ */
+
 #ifndef X11OSD_H
 #define X11OSD_H
 
