19706cf7934b976338b86137983dfafda8d5690d
Thibaut Mattern <tmattern@users.sourceforge.net>
2005-06-06 21:29:11 +0000

    Fixed frame duration.
    CVS patchset: 7614
    CVS date: 2005/06/06 21:29:11

--- src/libffmpeg/video_decoder.c
+++ src/libffmpeg/video_decoder.c
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
  *
- * $Id: video_decoder.c,v 1.52 2005/06/04 10:54:22 jstembridge Exp $
+ * $Id: video_decoder.c,v 1.53 2005/06/06 21:29:11 tmattern Exp $
  *
  * xine video decoder plugin using ffmpeg
  *
@@ -1215,6 +1215,18 @@ static void ff_decode_data (video_decoder_t *this_gen, buf_element_t *buf) {
   lprintf ("processing packet type = %08x, len = %d, decoder_flags=%08x\n", 
            buf->type, buf->size, buf->decoder_flags);
 
+  if (buf->decoder_flags & BUF_FLAG_FRAMERATE) {
+    this->video_step = buf->decoder_info[0];
+    _x_stream_info_set(this->stream, XINE_STREAM_INFO_FRAME_DURATION, this->video_step);
+  }
+  
+  if (buf->decoder_flags & BUF_FLAG_ASPECT) {
+    this->aspect_ratio = (double)buf->decoder_info[1] / (double)buf->decoder_info[2];
+    _x_stream_info_set(this->stream, XINE_STREAM_INFO_VIDEO_RATIO, 
+		       this->aspect_ratio*10000);
+  }  
+
+
   if (buf->decoder_flags & BUF_FLAG_PREVIEW) {
   
     ff_handle_preview_buffer(this, buf);
@@ -1231,21 +1243,8 @@ static void ff_decode_data (video_decoder_t *this_gen, buf_element_t *buf) {
 
       ff_handle_header_buffer(this, buf);
 
-    } 
-    
-    if (buf->decoder_flags & BUF_FLAG_FRAMERATE) {
-      this->video_step = buf->decoder_info[0];
-      _x_stream_info_set(this->stream, XINE_STREAM_INFO_FRAME_DURATION, this->video_step);
-    }
-    
-    if (buf->decoder_flags & BUF_FLAG_ASPECT) {
-      this->aspect_ratio = (double)buf->decoder_info[1] / (double)buf->decoder_info[2];
-      _x_stream_info_set(this->stream, XINE_STREAM_INFO_VIDEO_RATIO, 
-                         this->aspect_ratio*10000);
-    }
-    
-    if (!(buf->decoder_flags & BUF_FLAG_HEADER)) {
-  
+    } else {
+
       /* decode */
 
       /* aspect ratio */
