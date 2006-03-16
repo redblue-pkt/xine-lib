daaa227ccf4453be1b71dddcb6a9a0c2fc7e715b
Bastien Nocera <hadess@users.sourceforge.net>
2006-03-16 10:52:32 +0000

    - Also ignore the zero file size for character device nodes, patch from   Bill Fink <billfink@mindspring.com>
    CVS patchset: 7927
    CVS date: 2006/03/16 10:52:32

--- src/input/input_file.c
+++ src/input/input_file.c
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
  *
- * $Id: input_file.c,v 1.105 2005/10/14 21:02:16 miguelfreitas Exp $
+ * $Id: input_file.c,v 1.106 2006/03/16 10:52:32 hadess Exp $
  */
 
 #ifdef HAVE_CONFIG_H
@@ -293,9 +293,9 @@ static int file_plugin_open (input_plugin_t *this_gen ) {
     }
   }
 
-  /* don't check length of fifo */
+  /* don't check length of fifo or character device node */
   if (fstat (this->fh, &sbuf) == 0) {
-    if (S_ISFIFO(sbuf.st_mode))
+    if (S_ISFIFO(sbuf.st_mode) || S_ISCHR(sbuf.st_mode))
       return 1;
   }
 
