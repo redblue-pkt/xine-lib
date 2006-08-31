5271c78e911a4dc1bf76e4847ccc25bc7e2991c2
Diego 'Flameeyes' Petten? <flameeyes@gmail.com>
2006-08-31 22:16:53 +0000

    Move the help text out of the #if condition.
    CVS patchset: 8186
    CVS date: 2006/08/31 22:16:53

--- src/post/deinterlace/plugins/linearblend.c
+++ src/post/deinterlace/plugins/linearblend.c
@@ -302,14 +302,6 @@ static void deinterlace_scanline_linear_blend2_mmxext( uint8_t *output,
     emms();
 }
 
-static const char linearblendmethod_help[] =
-  "Avoids flicker by blurring consecutive frames of input.  Use this if "
-  "you want to run your monitor at an arbitrary refresh rate and not use "
-  "much CPU, and are willing to sacrifice detail.\n"
-  "\n"
-  "Temporal mode evenly blurs content for least flicker, but with visible "
-  "trails on fast motion. From the linear blend deinterlacer in mplayer.";
-
 static deinterlace_method_t linearblendmethod_mmxext =
 {
     "Linear Blend (mplayer)",
@@ -326,6 +318,14 @@ static deinterlace_method_t linearblendmethod_mmxext =
 };
 
 #endif
+
+static const char linearblendmethod_help[] =
+  "Avoids flicker by blurring consecutive frames of input.  Use this if "
+  "you want to run your monitor at an arbitrary refresh rate and not use "
+  "much CPU, and are willing to sacrifice detail.\n"
+  "\n"
+  "Temporal mode evenly blurs content for least flicker, but with visible "
+  "trails on fast motion. From the linear blend deinterlacer in mplayer.";
 
 static deinterlace_method_t linearblendmethod =
 {
