f075b7139d877e2f7ad98e7988aced629774bdeb
Diego 'Flameeyes' Petten? <flameeyes@gmail.com>
2007-01-19 02:49:31 +0000

    Revert, and remember to flush cache.
    CVS patchset: 8534
    CVS date: 2007/01/19 02:49:31

--- src/xine-engine/xine_plugin.h
+++ src/xine-engine/xine_plugin.h
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
  *
- * $Id: xine_plugin.h,v 1.22 2007/01/19 02:32:00 dgp85 Exp $
+ * $Id: xine_plugin.h,v 1.23 2007/01/19 02:49:31 dgp85 Exp $
  *
  * generic plugin definitions
  *
@@ -73,7 +73,7 @@ typedef struct {
 
 /* special_info for a decoder plugin */
 typedef struct {
-  const uint32_t          *supported_types;         /* streamtypes this decoder can handle       */
+  uint32_t          *supported_types;         /* streamtypes this decoder can handle       */
   int                      priority;
 } decoder_info_t;
 
