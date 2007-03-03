9ffea0c4e2f7cc1c9f4bbbaf49168a3586521ab6
Diego 'Flameeyes' Petten? <flameeyes@gmail.com>
2007-03-03 00:02:30 +0000

    Use LOG_MODULE in the log messages, so that during copy and paste I don't end up leaving demux_mpgaudio in demux_flac or demux_aac.
    CVS patchset: 8634
    CVS date: 2007/03/03 00:02:30

--- src/demuxers/demux_mpgaudio.c
+++ src/demuxers/demux_mpgaudio.c
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
  *
- * $Id: demux_mpgaudio.c,v 1.148 2007/02/20 00:34:56 dgp85 Exp $
+ * $Id: demux_mpgaudio.c,v 1.149 2007/03/03 00:02:30 dgp85 Exp $
  *
  * demultiplexer for mpeg audio (i.e. mp3) streams
  *
@@ -608,10 +608,10 @@ static int demux_mpgaudio_next (demux_mpgaudio_t *this, int decoder_flags, int s
         
       } else if ((BE_32(header_buf)) == ID3V22_TAG) {
         xprintf(this->stream->xine, XINE_VERBOSITY_LOG,
-                "demux_mpgaudio: ID3V2.2 tag\n");
+                LOG_MODULE ": ID3V2.2 tag\n");
         if (!id3v22_parse_tag(this->input, this->stream, header_buf)) {
           xprintf(this->stream->xine, XINE_VERBOSITY_LOG,
-                  "demux_mpgaudio: ID3V2.2 tag parsing error\n");
+                  LOG_MODULE ": ID3V2.2 tag parsing error\n");
           bytes = 1; /* resync */
         } else {
           bytes = 4;
@@ -619,10 +619,10 @@ static int demux_mpgaudio_next (demux_mpgaudio_t *this, int decoder_flags, int s
 
       } else if ((BE_32(header_buf)) == ID3V23_TAG) {
         xprintf(this->stream->xine, XINE_VERBOSITY_LOG,
-                "demux_mpgaudio: ID3V2.3 tag\n");
+                LOG_MODULE ": ID3V2.3 tag\n");
         if (!id3v23_parse_tag(this->input, this->stream, header_buf)) {
           xprintf(this->stream->xine, XINE_VERBOSITY_LOG,
-                  "demux_mpgaudio: ID3V2.3 tag parsing error\n");
+                  LOG_MODULE ": ID3V2.3 tag parsing error\n");
           bytes = 1; /* resync */
         } else {
           bytes = 4;
@@ -630,10 +630,10 @@ static int demux_mpgaudio_next (demux_mpgaudio_t *this, int decoder_flags, int s
 
       } else if ((BE_32(header_buf)) == ID3V24_TAG) {
         xprintf(this->stream->xine, XINE_VERBOSITY_LOG,
-                "demux_mpgaudio: ID3V2.4 tag\n");
+                LOG_MODULE ": ID3V2.4 tag\n");
         if (!id3v24_parse_tag(this->input, this->stream, header_buf)) {
           xprintf(this->stream->xine, XINE_VERBOSITY_LOG,
-                  "demux_mpgaudio: ID3V2.4 tag parsing error\n");
+                  LOG_MODULE ": ID3V2.4 tag parsing error\n");
           bytes = 1; /* resync */
         } else {
           bytes = 4;
