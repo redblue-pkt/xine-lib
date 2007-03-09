8f4643ae5ad6dd2b960b5082a980548aeb2979b2
Diego 'Flameeyes' Petten? <flameeyes@gmail.com>
2007-03-09 23:35:45 +0000

    Add stdio.h header to include, for Solaris compatibility. Patch by Albert Lee.
    CVS patchset: 8646
    CVS date: 2007/03/09 23:35:45

--- lib/asprintf.c
+++ lib/asprintf.c
@@ -22,6 +22,7 @@
 #include "config.h"
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <stdarg.h>
 
 int xine_private_vasprintf (char **buffer, const char *format, va_list ap)
