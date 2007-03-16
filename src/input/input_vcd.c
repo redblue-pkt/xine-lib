e1d9c409649334cfcbba8bdc73e21bc985439e64
Diego 'Flameeyes' Petten? <flameeyes@gmail.com>
2007-03-16 16:32:58 +0000

    Move netinet/in.h include down the block; this is currently patched by FreeBSD ports, and makes no functional change of sort on Linux.
    CVS patchset: 8675
    CVS date: 2007/03/16 16:32:58

--- src/input/input_vcd.c
+++ src/input/input_vcd.c
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
  *
- * $Id: input_vcd.c,v 1.88 2007/01/19 01:05:25 dgp85 Exp $
+ * $Id: input_vcd.c,v 1.89 2007/03/16 16:32:58 dgp85 Exp $
  *
  */
 
@@ -25,7 +25,6 @@
 #include "config.h"
 #endif
 
-#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
@@ -34,6 +33,7 @@
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <string.h>
+#include <netinet/in.h>
 #ifdef HAVE_LINUX_CDROM_H
 # include <linux/cdrom.h>
 #endif
