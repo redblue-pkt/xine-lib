e26b8fdc77de1ed621c05a51ae63da39aff96776
Darren Salt <linux@youmustbejoking.demon.co.uk>
2007-04-14 00:33:12 +0100

    Remove #ifdef wrappers from the Xv & XcbXv plugins. Allows XcbXv to be built.

--- src/video_out/video_out_xcbxv.c
+++ src/video_out/video_out_xcbxv.c
@@ -37,8 +37,6 @@
 #include "config.h"
 #endif
 
-#ifdef HAVE_XV
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -1618,5 +1616,3 @@ const plugin_info_t xine_plugin_info[] EXPORTED = {
   { PLUGIN_VIDEO_OUT, 21, "xv", XINE_VERSION_CODE, &vo_info_xv, init_class },
   { PLUGIN_NONE, 0, "", 0, NULL, NULL }
 };
-
-#endif
