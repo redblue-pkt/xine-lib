78705f2572e6b4c05ff6350cf69c15d3ee92f621
Darren Salt <linux@youmustbejoking.demon.co.uk>
2007-06-02 17:36:21 +0100

    Fiddle with the versioning so that we get libxine.so.2.0.0. This requires renaming po/libxine1.pot -> po/libxine2.pot.

--- version.sh
+++ version.sh
@@ -6,32 +6,31 @@
 #   3. Adjust the values of XINE_LT_CURRENT, XINE_LT_REVISION, and XINE_LT_AGE
 #      according to the following rules:
 #
-#          If the interface is totally unchanged from the previous release,
-#          increment XINE_LT_REVISION by one.  Otherwise:
-#            1. XINE_LT_REVISION=0
-#            2. Increment XINE_LT_CURRENT by one.
-#            3. If any interfaces have been ADDED since the last release,
-#               increment XINE_LT_AGE by one.  If any interfaces have been
-#               REMOVED or incompatibly changed, XINE_LT_AGE=0
+#      1. Increment XINE_LT_REVISION.
+#      2. If any interfaces have been added, removed, or changed, set
+#         XINE_LT_REVISION to 0 and increment XINE_LT_CURRENT.
+#      3. If any interfaces have been added, increment XINE_LT_AGE.
+#      4. If any interfaces have been removed or incompatibly changed,
+#         set XINE_LT_AGE to 0, and rename po/libxine*.pot accordingly
+#         (use "hg rename").
 #
-# Regarding libtool versioning, here are some details, but see the info page for
-# libtool for the whole story.  The most important thing to keep in mind is that
-# the libtool version numbers DO NOT MATCH the xine-lib version numbers, and you
-# should NEVER try to make them match.
+# The most important thing to keep in mind is that the libtool version
+# numbers DO NOT MATCH the xine-lib version numbers, and you should NEVER
+# try to make them match.
+#
+# See the libtool documentation for more information.
 #
 #   XINE_LT_CURRENT     the current API version
-#   XINE_LT_REVISION    an internal revision number that is increased when the
-#                       API does not change in any way
-#   XINE_LT_AGE         the number of previous API versions still supported by
-#                       this version
+#   XINE_LT_REVISION    the current revision of the current API
+#   XINE_LT_AGE         the number of previous API versions still supported
 
 XINE_VERSION_MAJOR=1
 XINE_VERSION_MINOR=1
 XINE_VERSION_SUB=90
 
-XINE_LT_CURRENT=19
+XINE_LT_CURRENT=2
 XINE_LT_REVISION=0
-XINE_LT_AGE=17
+XINE_LT_AGE=0
 
 test -f "`dirname $0`/.cvsversion" && XINE_VERSION_SUFFIX="hg"
 XINE_VERSION_SPEC="${XINE_VERSION_MAJOR}.${XINE_VERSION_MINOR}.${XINE_VERSION_SUB}${XINE_VERSION_SUFFIX}"
