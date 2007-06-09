89c5860846441cd2bd466c326375eaa6002ca419
Diego 'Flameeyes' Petten? <flameeyes@gmail.com>
2007-06-09 11:48:09 +0200

    Remove unused variable.

--- src/demuxers/demux_realaudio.c
+++ src/demuxers/demux_realaudio.c
@@ -313,8 +313,6 @@ static int demux_ra_get_status (demux_plugin_t *this_gen) {
 
 /* return the approximate length in miliseconds */
 static int demux_ra_get_stream_length (demux_plugin_t *this_gen) {
-  demux_ra_t *this = (demux_ra_t *) this_gen;
-
   return 0;
 }
 
