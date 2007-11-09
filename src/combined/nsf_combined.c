8d6b5b8670a3568df349e22cc536649a28f0bb5f
Diego 'Flameeyes' Petten? <flameeyes@gmail.com>
2007-11-09 18:17:08 +0100

    More updates for FSF address.

--- src/combined/nsf_combined.c
+++ src/combined/nsf_combined.c
@@ -15,7 +15,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA
  *
  * $Id: combined_wavpack.c,v 1.3 2007/03/17 07:34:02 dgp85 Exp $
  */
