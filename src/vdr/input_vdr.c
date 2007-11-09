8a06ce4a5104940d3e30437e85e68674ad6eb104
Diego 'Flameeyes' Petten? <flameeyes@gmail.com>
2007-11-09 18:17:08 +0100

    More updates for FSF address.

--- src/vdr/input_vdr.c
+++ src/vdr/input_vdr.c
@@ -15,7 +15,7 @@
  * 
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA
  *
  * $Id: input_vdr.c,v 1.51 2003/05/02 15:02:11 miguelfreitas Exp $
  */
