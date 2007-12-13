2877bf9e95eb71c33e8d96e7e39ac831189f4eb9
Diego 'Flameeyes' Petten? <flameeyes@gmail.com>
2007-12-13 15:23:58 +0100

    Use ME_FOURCC to define the ID3V2 tag macros.

--- src/demuxers/id3.h
+++ src/demuxers/id3.h
@@ -30,10 +30,10 @@
 #include "bswap.h"
 
 /* id3v2 */
-#define ID3V22_TAG        BE_FOURCC('I', 'D', '3', 2) /* id3 v2.2 header tag */
-#define ID3V23_TAG        BE_FOURCC('I', 'D', '3', 3) /* id3 v2.3 header tag */
-#define ID3V24_TAG        BE_FOURCC('I', 'D', '3', 4) /* id3 v2.4 header tag */
-#define ID3V24_FOOTER_TAG BE_FOURCC('3', 'D', 'I', 0) /* id3 v2.4 footer tag */
+#define ID3V22_TAG        ME_FOURCC('I', 'D', '3', 2) /* id3 v2.2 header tag */
+#define ID3V23_TAG        ME_FOURCC('I', 'D', '3', 3) /* id3 v2.3 header tag */
+#define ID3V24_TAG        ME_FOURCC('I', 'D', '3', 4) /* id3 v2.4 header tag */
+#define ID3V24_FOOTER_TAG ME_FOURCC('3', 'D', 'I', 0) /* id3 v2.4 footer tag */
 
 #define ID3V2X_TAG        ME_FOURCC('I', 'D', '3', 0) /* id3 v2.x header tag */
 #define ID3V2X_MASK      ~ME_FOURCC( 0 ,  0 ,  0 , 0xFF) /* id3 v2.x header mask */
