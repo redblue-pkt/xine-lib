c252b45f578e7637daf3368aab9654e271d99a21
Diego 'Flameeyes' Petten? <flameeyes@gmail.com>
2007-12-18 21:56:02 +0100

    Use calloc() when allocating an array of elements. (transplanted from e9e85d6bcc7e9aafb1dc019f3505de2dafe940bf)

--- src/input/libreal/real.c
+++ src/input/libreal/real.c
@@ -660,7 +660,7 @@ rmff_header_t  *real_setup_and_get_header(rtsp_t *rtsp_session, uint32_t bandwid
     
   lprintf("Stream description size: %i\n", size);
 
-  description = malloc(sizeof(char)*(size+1));
+  description = calloc(size+1, sizeof(char));
 
   if( rtsp_read_data(rtsp_session, description, size) <= 0) {
     xine_buffer_free(buf);
