99c1474f29b111ccaf024ba5ec96e497e3a827fb
Diego 'Flameeyes' Petten? <flameeyes@gmail.com>
2007-12-18 21:59:40 +0100

    Use strndup.

--- src/input/pnm.c
+++ src/input/pnm.c
@@ -21,6 +21,8 @@
  * based upon code from joschka
  */
 
+#include <config.h>
+
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/socket.h>
@@ -732,9 +734,7 @@ pnm_t *pnm_connect(xine_stream_t *stream, const char *mrl) {
   pathbegin=slash-mrl_ptr;
   hostend=colon-mrl_ptr;
 
-  p->host = malloc(sizeof(char)*hostend+1);
-  strncpy(p->host, mrl_ptr, hostend);
-  p->host[hostend]=0;
+  p->host = strndup(mrl_ptr, hostend);
 
   if (pathbegin < strlen(mrl_ptr)) p->path=strdup(mrl_ptr+pathbegin+1);
   if (colon != slash) {
