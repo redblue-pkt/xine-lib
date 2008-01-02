c8362a219822add672a701a06322547c8e8ac612
Diego 'Flameeyes' Petten? <flameeyes@gmail.com>
2008-01-02 18:46:49 +0100

    Mark id as constant, as we always use a literal.

--- include/xine/xine_plugin.h
+++ include/xine/xine_plugin.h
@@ -50,7 +50,7 @@
 typedef struct {
   uint8_t                  type;                    /* one of the PLUGIN_* constants above     */
   uint8_t                  API;                     /* API version supported by this plugin    */
-  char                    *id;                      /* a name that identifies this plugin      */
+  const char              *id;                      /* a name that identifies this plugin      */
   uint32_t                 version;                 /* version number, increased every release */
   const void              *special_info;            /* plugin-type specific, see structs below */
   void                  *(*init)(xine_t *, void *); /* init the plugin class            */
