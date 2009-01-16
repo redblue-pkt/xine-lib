399f2ae7f2aa2dfa71a03d5bd26d206f6d5bb9f9
Darren Salt <linux@youmustbejoking.demon.co.uk>
2009-01-16 16:51:30 +0000

    Back out MagickGetAuthenticPixels fix; causes FTBFS.

---
+++ src/input/vcd/libcdio/cdio/version.h
@@ -0,0 +1,10 @@
+/* $Id: version.h,v 1.2 2004/04/11 12:20:31 miguelfreitas Exp $ */
+/** \file version.h 
+ *  \brief  A file simply containing the library version number.
+ */
+
+/*! CDIO_VERSION can as a string in programs to show what version is used. */
+#define CDIO_VERSION "0.68"
+
+/*! LIBCDIO_VERSION_NUM  can be used for testing in the C preprocessor */
+#define LIBCDIO_VERSION_NUM 68
