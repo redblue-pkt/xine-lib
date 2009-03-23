a885e6a39e1ab00e9c5f8e0ffac59f0ae17256a7
Julian Scheel <julian@jusst.de>
2009-03-23 15:18:56 +0000

    Fix Sky Sports HD samples. pic_order_cnt calculation was broken in some cases.

--- src/libvdpau/vdpau_h264.c
+++ src/libvdpau/vdpau_h264.c
@@ -249,7 +249,7 @@ static void fill_vdpau_pictureinfo_h264(video_decoder_t *this_gen, uint32_t slic
   pic->field_pic_flag = slc->field_pic_flag;
   pic->bottom_field_flag = slc->bottom_field_flag;
   pic->num_ref_frames = sps->num_ref_frames;
-  pic->mb_adaptive_frame_field_flag = sps->mb_adaptive_frame_field_flag;
+  pic->mb_adaptive_frame_field_flag = sps->mb_adaptive_frame_field_flag && !slc->field_pic_flag;
   pic->constrained_intra_pred_flag = pps->constrained_intra_pred_flag;
   pic->weighted_pred_flag = pps->weighted_pred_flag;
   pic->weighted_bipred_idc = pps->weighted_bipred_idc;
