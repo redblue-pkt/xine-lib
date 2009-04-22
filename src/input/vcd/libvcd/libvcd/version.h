63784aa41932c55ba70927c898dbfe16a5a55dc9
Carlo Bramini <carlo.bramix@libero.it>
2009-04-22 18:30:00 +0000

    Demux timing fixes (Windows)
    m4/pthreads.m4
      * Mingw GCC says that '-pthread' option is unknown.
      * Correct library name under Mingw is -lpthreadGC2.
    src/xine-engine/demux.c
      * function _x_compute_interval cannot be compiled

---
+++ src/input/vcd/libvcd/libvcd/version.h
@@ -0,0 +1,11 @@
+/* $Id: version.h,v 1.4 2005/04/27 23:28:42 rockyb Exp $ */
+/** \file version.h 
+ *  \brief  A file simply containing the library version number.
+ */
+
+/*! LIBVCD_VERSION can as a string in programs to show what version is used. */
+#define LIBVCD_VERSION "0.7.21-"
+
+/*! LIBCDIO_VERSION_NUM  can be used for testing in the C preprocessor */
+#define LIBVCD_VERSION_NUM 21
+
