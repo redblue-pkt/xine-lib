b4c5f946ca12e3bab3c3a1b89f80cd7264bcc847
Petri Hintukainen <phintuka@users.sourceforge.net>
2009-08-31 01:34:16 +0100

    Fixed selecting HDMV SPU track

--- src/demuxers/demux_ts.c
+++ src/demuxers/demux_ts.c
@@ -519,6 +519,10 @@ static void demux_ts_update_spu_channel(demux_ts_t *this)
 #endif
     }
 
+ if ((this->media[this->spu_media].type & BUF_MAJOR_MASK) == BUF_SPU_HDMV) {
+   buf->type = BUF_SPU_HDMV;
+ }
+
   this->video_fifo->put(this->video_fifo, buf);
 }
 
