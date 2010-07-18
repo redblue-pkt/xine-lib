73f35f040a43eaa96457cbc1899237639043c7bd
Darren Salt <linux@youmustbejoking.demon.co.uk>
2010-07-18 23:48:13 +0100

    Fix playback of the first file handled by the modplug demuxer.

--- src/demuxers/demux_mod.c
+++ src/demuxers/demux_mod.c
@@ -166,13 +166,6 @@ static int open_mod_file(demux_mod_t *this) {
     return 0;
   }
 
-  this->mpfile = ModPlug_Load(this->buffer, this->filesize);
-  if (this->mpfile==NULL) {
-    xine_log(this->stream->xine, XINE_LOG_PLUGIN, "modplug - load error\n");
-    free(this->buffer);
-    return 0;
-  }
-
   /* Set up modplug engine */
   ModPlug_GetSettings(&this->settings);
   this->settings.mResamplingMode = MODPLUG_RESAMPLE_FIR; /* RESAMP */
@@ -180,6 +173,13 @@ static int open_mod_file(demux_mod_t *this) {
   this->settings.mBits = MOD_BITS;
   this->settings.mFrequency = MOD_SAMPLERATE;
   ModPlug_SetSettings(&this->settings);
+
+  this->mpfile = ModPlug_Load(this->buffer, this->filesize);
+  if (this->mpfile==NULL) {
+    xine_log(this->stream->xine, XINE_LOG_PLUGIN, "modplug - load error\n");
+    free(this->buffer);
+    return 0;
+  }
 
   this->title = strdup(ModPlug_GetName(this->mpfile));
   this->artist = strdup("");
