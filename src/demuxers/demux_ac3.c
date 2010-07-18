f017aa3eeaef69ff9044813965613614a5c46dc9
Darren Salt <linux@youmustbejoking.demon.co.uk>
2010-07-18 19:47:57 +0100

    Add audio/ac3.

--- src/demuxers/demux_ac3.c
+++ src/demuxers/demux_ac3.c
@@ -476,7 +476,7 @@ static const char *get_extensions (demux_class_t *this_gen) {
 }
 
 static const char *get_mimetypes (demux_class_t *this_gen) {
-  return NULL;
+  return "audio/ac3: ac3: Dolby Digital audio;";
 }
 
 static void class_dispose (demux_class_t *this_gen) {
