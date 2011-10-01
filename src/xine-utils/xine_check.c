64e5eb5144ac5c3b58ec5cb27fe9718624588cac
Chris Rankin <rankincj@yahoo.com>
2011-10-01 14:14:27 +0100

    Mark simple file and socket descriptors as uninheritable. This patch creates two utility functions:
      int open_cloexec(pathname, flags)
      int create_cloexec(pathname, flags, mode)
    
    These return a file descriptor with the CLOEXEC flag set, to ensure
    that the descriptor is not inherited across a fork/exec operation.
    
    The sockets returned by:
    
       _x_io_tcp_connect_ipv4()
       _x_io_tcp_connect()
    
    now also have their CLOEXEC flag set.

--- src/xine-utils/xine_check.c
+++ src/xine-utils/xine_check.c
@@ -168,7 +168,7 @@ static xine_health_check_t* _x_health_check_cdrom (xine_health_check_t* hc) {
     return hc;
   }
 
-  if ( (fd = open(hc->cdrom_dev, O_RDWR)) < 0) {
+  if ( (fd = open(hc->cdrom_dev, O_RDWR | O_CLOEXEC)) < 0) {
     switch (errno) {
     case EACCES:
       set_hc_result (hc, XINE_HEALTH_CHECK_FAIL, "FAILED - %s permissions are not sufficient\n.", hc->cdrom_dev);
@@ -204,7 +204,7 @@ static xine_health_check_t* _x_health_check_dvdrom(xine_health_check_t* hc) {
     return hc;
   }
 
-  if ( (fd = open(hc->dvd_dev, O_RDWR)) < 0) {
+  if ( (fd = open(hc->dvd_dev, O_RDWR | O_CLOEXEC)) < 0) {
     switch (errno) {
     case EACCES:
       set_hc_result (hc, XINE_HEALTH_CHECK_FAIL, "FAILED - %s permissions are not sufficient\n.", hc->dvd_dev);
@@ -247,7 +247,7 @@ static xine_health_check_t* _x_health_check_dma (xine_health_check_t* hc) {
     return hc;
   }
 
-  fd = open (hc->dvd_dev, O_RDONLY | O_NONBLOCK);
+  fd = open (hc->dvd_dev, O_RDONLY | O_NONBLOCK | O_CLOEXEC);
   if (fd < 0) {
     set_hc_result(hc, XINE_HEALTH_CHECK_FAIL, "FAILED - Could not open %s.\n", hc->dvd_dev);
     return hc;
