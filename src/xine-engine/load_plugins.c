8b724c81f2b3bc5b617b9bb7bc10695ce041fec3
Petri Hintukainen <phintuka@users.sourceforge.net>
2011-12-16 11:15:58 +0200

    Removed write-only variables

--- src/xine-engine/load_plugins.c
+++ src/xine-engine/load_plugins.c
@@ -1662,7 +1662,6 @@ xine_video_port_t *xine_new_framegrab_video_port (xine_t *this) {
   plugin_node_t      *node;
   vo_driver_t        *driver;
   xine_video_port_t  *port;
-  vo_info_t          *vo_info;
   plugin_catalog_t   *catalog = this->plugin_catalog;
   char               *id;
   int                 list_id, list_size;
@@ -1677,7 +1676,6 @@ xine_video_port_t *xine_new_framegrab_video_port (xine_t *this) {
 
     node = xine_sarray_get (catalog->plugin_lists[PLUGIN_VIDEO_OUT - 1], list_id);
 
-    vo_info = (vo_info_t *)node->info->special_info;
     if (!strcasecmp (node->info->id, id)) {
       driver = _load_video_driver (this, node, NULL);
       break;
@@ -1772,7 +1770,6 @@ ao_driver_t *_x_load_audio_output_plugin (xine_t *this, const char *id)
 {
   plugin_node_t      *node;
   ao_driver_t        *driver = NULL;
-  ao_info_t          *ao_info;
   plugin_catalog_t   *catalog = this->plugin_catalog;
   int                 list_id, list_size;
 
@@ -1782,8 +1779,6 @@ ao_driver_t *_x_load_audio_output_plugin (xine_t *this, const char *id)
   for (list_id = 0; list_id < list_size; list_id++) {
 
     node = xine_sarray_get (this->plugin_catalog->plugin_lists[PLUGIN_AUDIO_OUT - 1], list_id);
-
-    ao_info = (ao_info_t *)node->info->special_info;
 
     if (!strcasecmp(node->info->id, id)) {
       driver = _load_audio_driver (this, node, NULL);
