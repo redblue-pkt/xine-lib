bf1b451335d61b6a47231ceff144e668ff9fdc4b
Petri Hintukainen <phintuka@users.sourceforge.net>
2012-01-16 22:21:37 +0200

    Removed unused variables and code

--- src/demuxers/demux_mpeg_block.c
+++ src/demuxers/demux_mpeg_block.c
@@ -868,6 +868,7 @@ static int32_t parse_private_stream_1(demux_mpeg_block_t *this, uint8_t *p, buf_
     } else if ((p[0]&0xf0) == 0xa0) {
 
       int pcm_offset;
+#if 0
       int number_of_frame_headers;
       int first_access_unit_pointer;
       int audio_frame_number;
@@ -875,6 +876,7 @@ static int32_t parse_private_stream_1(demux_mpeg_block_t *this, uint8_t *p, buf_
       int sample_rate;
       int num_channels;
       int dynamic_range;
+#endif
 
       /*
        * found in http://members.freemail.absa.co.za/ginggs/dvd/mpeg2_lpcm.txt
@@ -882,6 +884,7 @@ static int32_t parse_private_stream_1(demux_mpeg_block_t *this, uint8_t *p, buf_
        */
 
       track = p[0] & 0x0F;
+#if 0
       number_of_frame_headers = p[1];
       /* unknown = p[2]; */
       first_access_unit_pointer = p[3];
@@ -906,6 +909,7 @@ static int32_t parse_private_stream_1(demux_mpeg_block_t *this, uint8_t *p, buf_
       case 2: bits_per_sample = 24; break;
       }
       dynamic_range = p[6];
+#endif
 
       /* send lpcm config byte */
       buf->decoder_flags |= BUF_FLAG_SPECIAL;
