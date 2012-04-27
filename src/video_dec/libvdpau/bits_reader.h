3e0a5dca91c55a555191d781b163ee72c9e81e1a
Petri Hintukainen <phintuka@users.sourceforge.net>
2012-04-27 17:07:35 +0300

    Factorized duplicate code

--- src/video_dec/libvdpau/bits_reader.h
+++ src/video_dec/libvdpau/bits_reader.h
@@ -40,34 +40,6 @@ static void bits_reader_set( bits_reader_t *br, uint8_t *buf, int len )
 
 
 
-static uint32_t read_bits( bits_reader_t *br, int nbits )
-{
-  int i, nbytes;
-  uint32_t ret = 0;
-  uint8_t *buf;
-
-  buf = br->buffer;
-  nbytes = (br->offbits + nbits)/8;
-  if ( ((br->offbits + nbits) %8 ) > 0 )
-    nbytes++;
-  if ( (buf + nbytes) > (br->start + br->length) ) {
-    br->oflow = 1;
-    return 0;
-  }
-  for ( i=0; i<nbytes; i++ )
-    ret += buf[i]<<((nbytes-i-1)*8);
-  i = (4-nbytes)*8+br->offbits;
-  ret = ((ret<<i)>>i)>>((nbytes*8)-nbits-br->offbits);
-
-  br->offbits += nbits;
-  br->buffer += br->offbits / 8;
-  br->offbits %= 8;
-
-  return ret;
-}
-
-
-
 static void skip_bits( bits_reader_t *br, int nbits )
 {
   br->offbits += nbits;
@@ -98,6 +70,19 @@ static uint32_t get_bits( bits_reader_t *br, int nbits )
     ret += buf[i]<<((nbytes-i-1)*8);
   i = (4-nbytes)*8+br->offbits;
   ret = ((ret<<i)>>i)>>((nbytes*8)-nbits-br->offbits);
+
+  return ret;
+}
+
+
+
+static uint32_t read_bits( bits_reader_t *br, int nbits )
+{
+  uint32_t ret = get_bits(br, nbits);
+
+  br->offbits += nbits;
+  br->buffer += br->offbits / 8;
+  br->offbits %= 8;
 
   return ret;
 }
