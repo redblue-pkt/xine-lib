230915b1dda034a423e78c607ef06a76a0947667
Torsten Jager <t.jager@gmx.de>
2013-09-30 15:24:55 +0200

    post.h: fix build regression #12387 and avoid nonportable __typeof__.

--- include/xine/post.h
+++ include/xine/post.h
@@ -392,12 +392,17 @@ do {                                                               \
 /* macros to create parameter descriptors */
 
 #define START_PARAM_DESCR( param_t ) \
+typedef param_t temp_t; \
 static param_t temp_s; \
 static xine_post_api_parameter_t temp_p[] = {
 
+#ifndef offsetof
+#include <stddef.h>
+#endif
+
 #define PARAM_ITEM( param_type, var, enumv, min, max, readonly, descr ) \
 { param_type, #var, sizeof(temp_s.var), \
-  offsetof(__typeof__(temp_s), var), enumv, min, max, readonly, descr },
+  offsetof(temp_t, var), enumv, min, max, readonly, descr },
 
 #define END_PARAM_DESCR( name ) \
   { POST_PARAM_TYPE_LAST, NULL, 0, 0, NULL, 0, 0, 1, NULL } \
