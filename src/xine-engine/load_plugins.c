a3b7c3c21688287799f6e69bbcecdba9feda2cbb
Torsten Jager <t.jager@gmx.de>
2016-12-10 17:59:40 +0100

    Deglobalize xine flags.
    I dont quite get why Darren made that global back in 2012.
    The private tail of xine_t is not part of public API/ABI I think.
    It has been added to several times before, too.
    
    More important, this does possibly enable build with the combination of
    gcc < 5.0 and gnu_ld >= 2.26.
    
    /usr/lib64/gcc/x86_64-suse-linux/4.8/../../../../x86_64-suse-linux/bin/ld:
    .libs/xine.o: relocation R_X86_64_PC32 against protected symbol
    `_x_flags' can not be used when making a shared object
    
    Thanks to Joerg Riechardt <J.Riechardt@gmx.de> for reporting and testing.

--- src/xine-engine/load_plugins.c
+++ src/xine-engine/load_plugins.c
@@ -1326,7 +1326,7 @@ void _x_scan_plugins (xine_t *this) {
 
   load_required_plugins (this);
 
-  if ((_x_flags & XINE_FLAG_NO_WRITE_CACHE) == 0)
+  if ((this->flags & XINE_FLAG_NO_WRITE_CACHE) == 0)
     XINE_PROFILE(save_catalog (this));
 
   map_decoders (this);
