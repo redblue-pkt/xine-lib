9e7dead32b90deed4f1eb55267d00a8f15ad8476
Petri Hintukainen <phintuka@users.sourceforge.net>
2017-03-20 10:57:23 +0200

    Add missing copyright/license header to asfheader.c

--- src/demuxers/asfheader.c
+++ src/demuxers/asfheader.c
@@ -1,3 +1,32 @@
+/*
+ * Copyright (C) 2000-2017 the xine project
+ *
+ * This file is part of xine, a free video player.
+ *
+ * xine is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * xine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA
+ *
+ * demultiplexer for asf streams
+ *
+ * based on ffmpeg's
+ * ASF compatible encoder and decoder.
+ * Copyright (c) 2000, 2001 Gerard Lantau.
+ *
+ * GUID list from avifile
+ * some other ideas from MPlayer
+ */
+
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
