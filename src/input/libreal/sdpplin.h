4731dbf6d1827a537da764780cf69875f6191f8a
Petri Hintukainen <phintuka@users.sourceforge.net>
2017-03-21 15:30:12 +0200

    Remove unused data

--- src/input/libreal/sdpplin.h
+++ src/input/libreal/sdpplin.h
@@ -35,22 +35,22 @@
 typedef struct {
 
   char *id;
-  char *bandwidth;
+  //char *bandwidth;
 
   uint16_t stream_id;
-  char *range;
-  char *length;
-  char *rtpmap;
-  char *mimetype;
-  int min_switch_overlap;
+  //char *range;
+  //char *length;
+  //char *rtpmap;
+  //char *mimetype;
+  //int min_switch_overlap;
   int start_time;
-  int end_one_rule_end_all;
+  //int end_one_rule_end_all;
   int avg_bit_rate;
   int max_bit_rate;
   int avg_packet_size;
   int max_packet_size;
-  int end_time;
-  int seek_greater_on_switch;
+  //int end_time;
+  //int seek_greater_on_switch;
   int preroll;
 
   int duration;
@@ -60,8 +60,8 @@ typedef struct {
   int mime_type_size;
   char *mlti_data;
   int mlti_data_size;
-  int  rmff_flags_length;
-  char *rmff_flags;
+  //int  rmff_flags_length;
+  //char *rmff_flags;
   int  asm_rule_book_length;
   char *asm_rule_book;
 
@@ -69,33 +69,33 @@ typedef struct {
 
 typedef struct {
 
-  int sdp_version, sdpplin_version;
-  char *owner;
-  char *session_name;
-  char *session_info;
-  char *uri;
-  char *email;
-  char *phone;
-  char *connection;
-  char *bandwidth;
+  //int sdp_version, sdpplin_version;
+  //char *owner;
+  //char *session_name;
+  //char *session_info;
+  //char *uri;
+  //char *email;
+  //char *phone;
+  //char *connection;
+  //char *bandwidth;
 
   int flags;
-  int is_real_data_type;
+  //int is_real_data_type;
   uint16_t stream_count;
   char *title;
   char *author;
   char *copyright;
-  char *keywords;
-  int  asm_rule_book_length;
-  char *asm_rule_book;
+  //char *keywords;
+  //int  asm_rule_book_length;
+  //char *asm_rule_book;
   char *abstract;
-  char *range;
-  int avg_bit_rate;
-  int max_bit_rate;
-  int avg_packet_size;
-  int max_packet_size;
-  int preroll;
-  int duration;
+  //char *range;
+  //int avg_bit_rate;
+  //int max_bit_rate;
+  //int avg_packet_size;
+  //int max_packet_size;
+  //int preroll;
+  //int duration;
 
   sdpplin_stream_t **stream;
 
