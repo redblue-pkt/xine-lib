de23268334feb4cb424cb0e6cd83a87d1a9253af
Petri Hintukainen <phintuka@users.sourceforge.net>
2017-04-07 14:08:13 +0300

    Add missing file header

--- src/video_out/yuv2rgb.h
+++ src/video_out/yuv2rgb.h
@@ -1,3 +1,28 @@
+/*
+ * yuv2rgb.h
+ *
+ * Copyright (C) 2001-2017 the xine project
+ * This file is part of xine, a free video player.
+ *
+ * based on work from mpeg2dec:
+ * Copyright (C) 1999-2001 Aaron Holtzman <aholtzma@ess.engr.uvic.ca>
+ *
+ * This file is part of mpeg2dec, a free MPEG-2 video stream decoder.
+ *
+ * mpeg2dec is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * mpeg2dec is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA
+ */
 
 #ifndef HAVE_YUV2RGB_H
 #define HAVE_YUV2RGB_H
