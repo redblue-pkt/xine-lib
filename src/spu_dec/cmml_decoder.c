5150078a4c6f4386469967cf8d3b79544fd341cd
Petri Hintukainen <phintuka@users.sourceforge.net>
2017-04-12 15:57:23 +0300

    include config.h

--- src/spu_dec/cmml_decoder.c
+++ src/spu_dec/cmml_decoder.c
@@ -18,6 +18,10 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA
  */
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #define LOG_MODULE "libspucmml"
 #define LOG_VERBOSE
 /*
