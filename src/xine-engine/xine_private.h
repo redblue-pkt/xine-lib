1cefded1ed6b3d4c096e51f11aedd7632e5ce33c
Petri Hintukainen <phintuka@users.sourceforge.net>
2017-10-03 16:41:45 +0300

    Disable deprecation warnings in selected places
    (where deprecated features are used for backwards compatibiity)

--- src/xine-engine/xine_private.h
+++ src/xine-engine/xine_private.h
@@ -44,6 +44,14 @@
 # define INTERNAL
 #endif
 
+#if defined (__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 6 ))
+#    define XINE_DISABLE_DEPRECATION_WARNINGS _Pragma("GCC diagnostic ignored \"-Wdeprecated-declarations\"")
+#    define XINE_ENABLE_DEPRECATION_WARNINGS  _Pragma("GCC diagnostic warning \"-Wdeprecated-declarations\"")
+#else
+#    define XINE_DISABLE_DEPRECATION_WARNINGS
+#    define XINE_ENABLE_DEPRECATION_WARNINGS
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
