0ee53d5d9f2232b2d3429b7527af898f7cb27d1c
Petri Hintukainen <phintuka@users.sourceforge.net>
2017-10-11 13:32:14 +0300

    configfile.h: fix compilation when included in c++

--- include/xine/configfile.h
+++ include/xine/configfile.h
@@ -236,7 +236,7 @@ config_values_t *_x_config_init (void) XINE_MALLOC;
 
 int _x_config_change_opt(config_values_t *config, const char *opt);
 
-void _x_config_unregister_cb_class(config_values_t *this, void *callback_data) XINE_PROTECTED;
+void _x_config_unregister_cb_class(config_values_t *config, void *callback_data) XINE_PROTECTED;
 
 #ifdef __cplusplus
 }
