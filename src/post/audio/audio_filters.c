2e784ea72526a9271f186b90bd5bd862377eae60
Petri Hintukainen <phintuka@users.sourceforge.net>
2018-01-13 14:00:22 +0200

    Reduce static data

--- src/post/audio/audio_filters.c
+++ src/post/audio/audio_filters.c
@@ -31,17 +31,14 @@
 #include "audio_filters.h"
 
 
-static const post_info_t upmix_special_info      = { XINE_POST_TYPE_AUDIO_FILTER };
-static const post_info_t upmix_mono_special_info = { XINE_POST_TYPE_AUDIO_FILTER };
-static const post_info_t stretch_special_info    = { XINE_POST_TYPE_AUDIO_FILTER };
-static const post_info_t volnorm_special_info    = { XINE_POST_TYPE_AUDIO_FILTER };
+static const post_info_t gen_special_info = { XINE_POST_TYPE_AUDIO_FILTER };
 
 
 const plugin_info_t xine_plugin_info[] EXPORTED = {
   /* type, API, "name", version, special_info, init_function */
-  { PLUGIN_POST, 10, "upmix",      XINE_VERSION_CODE, &upmix_special_info,      &upmix_init_plugin },
-  { PLUGIN_POST, 10, "upmix_mono", XINE_VERSION_CODE, &upmix_mono_special_info, &upmix_mono_init_plugin },
-  { PLUGIN_POST, 10, "stretch",    XINE_VERSION_CODE, &stretch_special_info,    &stretch_init_plugin },
-  { PLUGIN_POST, 10, "volnorm",    XINE_VERSION_CODE, &volnorm_special_info,    &volnorm_init_plugin },
+  { PLUGIN_POST, 10, "upmix",      XINE_VERSION_CODE, &gen_special_info,        &upmix_init_plugin },
+  { PLUGIN_POST, 10, "upmix_mono", XINE_VERSION_CODE, &gen_special_info,        &upmix_mono_init_plugin },
+  { PLUGIN_POST, 10, "stretch",    XINE_VERSION_CODE, &gen_special_info,        &stretch_init_plugin },
+  { PLUGIN_POST, 10, "volnorm",    XINE_VERSION_CODE, &gen_special_info,        &volnorm_init_plugin },
   { PLUGIN_NONE, 0,  "",           0,                 NULL,                     NULL }
 };
