443055d81a9a86615158d705cbbf3e809d445cfc
Torsten Jager <t.jager@gmx.de>
2018-02-24 18:09:50 +0100

    Replace "obsolete" gettimeofday () 2.

--- lib/gettimeofday.c
+++ lib/gettimeofday.c
@@ -2,14 +2,30 @@
 
 #include "config.h"
 
-#include <sys/timeb.h>
-#ifdef WIN32
-#include <winsock.h>
+#ifdef HAVE_POSIX_TIMERS
+#  include <time.h>
 #else
-#include <sys/time.h>
+#  include <sys/timeb.h>
+#endif
+
+#ifdef WIN32
+#  include <winsock.h>
+#else
+#  include <sys/time.h>
 #endif
 
 int xine_private_gettimeofday(struct timeval *tv) {
+#ifdef HAVE_POSIX_TIMERS
+  struct timespec *ts;
+  int r;
+  r = clock_gettime (CLOCK_REALTIME, &ts);
+  if (!r) {
+    tv->tv_sec  = ts.tv_sec;
+    tv->tv_usec = ts.tv_nsec / 1000;
+  }
+  return r;
+}
+#else
   struct timeb tp;
 
   ftime(&tp);
@@ -17,4 +33,5 @@ int xine_private_gettimeofday(struct timeval *tv) {
   tv->tv_usec = tp.millitm * 1000;
 
   return 0;
+#endif
 }
