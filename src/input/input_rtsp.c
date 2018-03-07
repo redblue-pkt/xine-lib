fef59a0296a314986c5259db7a543e749d518f9d
Petri Hintukainen <phintuka@users.sourceforge.net>
2018-03-07 13:09:09 +0200

    input/group_network: use generic read_block

--- src/input/input_rtsp.c
+++ src/input/input_rtsp.c
@@ -53,6 +53,7 @@
 
 #include "librtsp/rtsp_session.h"
 #include "net_buf_ctrl.h"
+#include "input_helper.h"
 #include "group_network.h"
 
 #define BUFSIZE 1025
@@ -88,36 +89,6 @@ static off_t rtsp_plugin_read (input_plugin_t *this_gen,
     this->curpos += n;
 
   return n;
-}
-
-static buf_element_t *rtsp_plugin_read_block (input_plugin_t *this_gen,
-                                             fifo_buffer_t *fifo, off_t todo) {
-  /*rtsp_input_plugin_t   *this = (rtsp_input_plugin_t *) this_gen; */
-  buf_element_t        *buf = fifo->buffer_pool_alloc (fifo);
-  int                   total_bytes;
-
-  lprintf ("rtsp_plugin_read_block: %"PRId64" bytes...\n", todo);
-
-  if (todo > buf->max_size)
-    todo = buf->max_size;
-  if (todo < 0) {
-    buf->free_buffer (buf);
-    return NULL;
-  }
-
-  buf->content = buf->mem;
-  buf->type = BUF_DEMUX_BLOCK;
-
-  total_bytes = rtsp_plugin_read (this_gen, (char*)buf->content, todo);
-
-  if (total_bytes != todo) {
-    buf->free_buffer (buf);
-    return NULL;
-  }
-
-  buf->size = total_bytes;
-
-  return buf;
 }
 
 static off_t rtsp_plugin_seek (input_plugin_t *this_gen, off_t offset, int origin) {
@@ -170,10 +141,6 @@ static off_t rtsp_plugin_get_length (input_plugin_t *this_gen) {
 
 static uint32_t rtsp_plugin_get_capabilities (input_plugin_t *this_gen) {
   return INPUT_CAP_PREVIEW | INPUT_CAP_RIP_FORBIDDEN;
-}
-
-static uint32_t rtsp_plugin_get_blocksize (input_plugin_t *this_gen) {
-  return 0;
 }
 
 static off_t rtsp_plugin_get_current_pos (input_plugin_t *this_gen){
@@ -273,12 +240,12 @@ static input_plugin_t *rtsp_class_get_instance (input_class_t *cls_gen, xine_str
   this->input_plugin.open              = rtsp_plugin_open;
   this->input_plugin.get_capabilities  = rtsp_plugin_get_capabilities;
   this->input_plugin.read              = rtsp_plugin_read;
-  this->input_plugin.read_block        = rtsp_plugin_read_block;
+  this->input_plugin.read_block        = _x_input_default_read_block;
   this->input_plugin.seek              = rtsp_plugin_seek;
   this->input_plugin.seek_time         = rtsp_plugin_seek_time;
   this->input_plugin.get_current_pos   = rtsp_plugin_get_current_pos;
   this->input_plugin.get_length        = rtsp_plugin_get_length;
-  this->input_plugin.get_blocksize     = rtsp_plugin_get_blocksize;
+  this->input_plugin.get_blocksize     = _x_input_default_get_blocksize;
   this->input_plugin.get_mrl           = rtsp_plugin_get_mrl;
   this->input_plugin.dispose           = rtsp_plugin_dispose;
   this->input_plugin.get_optional_data = rtsp_plugin_get_optional_data;
