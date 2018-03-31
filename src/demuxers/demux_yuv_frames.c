3a3d69bf3911f869acea29bb20393e1bb7944755
Petri Hintukainen <phintuka@users.sourceforge.net>
2018-03-31 23:42:48 +0300

    Check for error

--- src/demuxers/demux_yuv_frames.c
+++ src/demuxers/demux_yuv_frames.c
@@ -198,7 +198,10 @@ static demux_plugin_t *open_plugin (demux_class_t *class_gen,
    * if we reach this point, the input has been accepted.
    */
 
-  this         = calloc(1, sizeof(demux_yuv_frames_t));
+  this = calloc(1, sizeof(demux_yuv_frames_t));
+  if (!this)
+    return NULL;
+
   this->stream = stream;
   this->input  = input;
 
