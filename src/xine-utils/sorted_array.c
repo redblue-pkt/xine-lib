8813b7256bbc9087c0ea66871598123f2886b364
Torsten Jager <t.jager@gmx.de>
2018-08-10 20:21:10 +0200

    Rewrite sorted array helper.  - Drop array class dependency.  - Embed initial buf.  - Simplify binary search.  - Add mode flags.

--- src/xine-utils/sorted_array.c
+++ src/xine-utils/sorted_array.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2000-2009 the xine project
+ * Copyright (C) 2000-2018 the xine project
  *
  * This file is part of xine, a free video player.
  *
@@ -16,6 +16,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA
+ *
+ * rewritten by Torsten Jager <t.jager@gmx.de>
  */
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -26,91 +28,180 @@
 #include <xine/attributes.h>
 #include <xine/sorted_array.h>
 
+#define MIN_CHUNK_SIZE 64
+
 /* Array internal struct */
 struct xine_sarray_s {
-  xine_array_t             *array;
+  void                    **chunk;
+  size_t                    chunk_size;
+  size_t                    size;
   xine_sarray_comparator_t  comparator;
+  unsigned int              mode;
+  void                     *default_chunk[1];
 };
 
 /* Constructor */
-xine_sarray_t *xine_sarray_new(size_t initial_size, xine_sarray_comparator_t comparator) {
+xine_sarray_t *xine_sarray_new (size_t initial_size, xine_sarray_comparator_t comparator) {
   xine_sarray_t *new_sarray;
 
-  new_sarray = (xine_sarray_t *)malloc(sizeof(xine_sarray_t));
+  if (initial_size == 0)
+    initial_size = MIN_CHUNK_SIZE;
+  new_sarray = malloc (sizeof (*new_sarray) + (initial_size - 1) * sizeof (void *));
   if (!new_sarray)
     return NULL;
-
-  new_sarray->array = xine_array_new(initial_size);
+  new_sarray->chunk_size = initial_size;
   new_sarray->comparator = comparator;
-
+  new_sarray->chunk      = &new_sarray->default_chunk[0];
+  new_sarray->size       = 0;
+  new_sarray->mode       = XINE_SARRAY_MODE_DEFAULT;
   return new_sarray;
 }
 
 /* Destructor */
-void xine_sarray_delete(xine_sarray_t *sarray) {
-  if (sarray->array) {
-    xine_array_delete(sarray->array);
+void xine_sarray_delete (xine_sarray_t *sarray) {
+  if (sarray) {
+    if (sarray->chunk != &sarray->default_chunk[0])
+      free (sarray->chunk);
+    free (sarray);
   }
-  free(sarray);
 }
 
-size_t xine_sarray_size(const xine_sarray_t *sarray) {
-  return xine_array_size(sarray->array);
+size_t xine_sarray_size (const xine_sarray_t *sarray) {
+  return sarray ? sarray->size : 0;
 }
 
-void xine_sarray_clear(xine_sarray_t *sarray) {
-  xine_array_clear(sarray->array);
+void xine_sarray_set_mode (xine_sarray_t *sarray, unsigned int mode) {
+  if (sarray)
+    sarray->mode = mode;
 }
 
-int xine_sarray_add(xine_sarray_t *sarray, void *value) {
-  int pos;
-
-  pos = xine_sarray_binary_search(sarray, value);
-  if (pos < 0)
-    pos = ~pos;
-  xine_array_insert(sarray->array, pos, value);
-
-  return pos;
+void *xine_sarray_get (xine_sarray_t *sarray, unsigned int position) {
+  if (sarray) {
+    if (position < sarray->size)
+      return sarray->chunk[position];
+  }
+  return NULL;
 }
 
-void xine_sarray_remove(xine_sarray_t *sarray, unsigned int position) {
-  xine_array_remove(sarray->array, position);
+void xine_sarray_clear (xine_sarray_t *sarray) {
+  if (sarray)
+    sarray->size = 0;
 }
 
-void *xine_sarray_get(xine_sarray_t *sarray, unsigned int position) {
-  return xine_array_get(sarray->array, position);
+void xine_sarray_remove (xine_sarray_t *sarray, unsigned int position) {
+  if (sarray) {
+    if (position < sarray->size) {
+      void **here = sarray->chunk + position;
+      unsigned int u = sarray->size - position - 1;
+      while (u--) {
+        here[0] = here[1];
+        here++;
+      }
+      sarray->size--;
+    }
+  }
+}
+
+static void _xine_sarray_insert (xine_sarray_t *sarray, unsigned int pos, void *value) {
+  if (sarray->size + 1 > sarray->chunk_size) {
+    size_t new_size;
+    void **new_chunk;
+
+    new_size = 2 * (sarray->size + 1);
+    if (new_size < MIN_CHUNK_SIZE)
+      new_size = MIN_CHUNK_SIZE;
+    if (sarray->chunk == &sarray->default_chunk[0]) {
+      new_chunk = malloc (new_size * sizeof (void *));
+      if (!new_chunk)
+        return;
+      memcpy (new_chunk, sarray->chunk, sarray->size * sizeof (void *));
+    } else {
+      new_chunk = realloc (sarray->chunk, new_size * sizeof (void *));
+      if (!new_chunk)
+        return;
+    }
+    sarray->chunk = new_chunk;
+    sarray->chunk_size = new_size;
+  }
+  if (pos <= sarray->size) {
+    void **here = sarray->chunk + sarray->size;
+    unsigned int u = sarray->size - pos;
+    while (u--) {
+      here[0] = here[-1];
+      here--;
+    }
+    here[0] = value;
+    sarray->size++;
+  }
+}
+
+int xine_sarray_add (xine_sarray_t *sarray, void *value) {
+  if (sarray) {
+    int pos1;
+    unsigned int pos2;
+
+    pos1 = xine_sarray_binary_search (sarray, value);
+    if ((pos1 >= 0) && (sarray->mode & XINE_SARRAY_MODE_UNIQUE))
+      return ~pos1;
+    pos2 = pos1 < 0 ? ~pos1 : pos1;
+    _xine_sarray_insert (sarray, pos2, value);
+    return pos2;
+  }
+  return 0;
 }
 
 int xine_sarray_binary_search(xine_sarray_t *sarray, void *key) {
-  int low, high, mid, pos;
-  int comp;
+  unsigned int b, e, m;
 
-  if (xine_array_size(sarray->array) > 0) {
-    low = 0;
-    high = xine_array_size(sarray->array) - 1;
+  if (!sarray)
+    return ~0; /* not found */
+  if (sarray->size == 0)
+    return ~0; /* not found */
 
-    while ((high - low ) > 1) {
-      mid = low + (high - low) / 2;
-      if (sarray->comparator(key, xine_array_get(sarray->array, mid)) < 0) {
-        high = mid;
-      } else {
-        low = mid;
-      }
+  b = 0; e = sarray->size; m = (b + e) >> 1;
+
+  if (sarray->mode & (XINE_SARRAY_MODE_FIRST | XINE_SARRAY_MODE_LAST)) {
+    int found = 0;
+    if (sarray->mode & XINE_SARRAY_MODE_FIRST) {
+      do {
+        int d = sarray->comparator (key, sarray->chunk[m]);
+        if (d == 0) {
+          found = 1;
+          e = m;
+        } else if (d < 0) {
+          e = m;
+        } else {
+          b = m + 1;
+        }
+        m = (b + e) >> 1;
+      } while (b != e);
+    } else { /* XINE_SARRAY_MODE_LAST */
+      do {
+        int d = sarray->comparator (key, sarray->chunk[m]);
+        if (d == 0) {
+          found = 1;
+          b = m + 1;
+        } else if (d < 0) {
+          e = m;
+        } else {
+          b = m + 1;
+        }
+        m = (b + e) >> 1;
+      } while (b != e);
     }
-
-    if ((comp = sarray->comparator(key, xine_array_get(sarray->array, low))) < 0) {
-      pos = ~low;         /* not found */
-    } else if (comp == 0) {
-      pos = low;          /* found */
-    } else if ((comp = sarray->comparator(key, xine_array_get(sarray->array, high))) < 0) {
-      pos = ~high;        /* not found */
-    } else if (comp == 0) {
-      pos = high;         /* found */
-    } else {
-      pos = ~(high + 1);  /* not found */
-    }
-  } else {
-    pos = ~0; /* not found */
+    return found ? m : ~m;
   }
-  return pos;
+
+  /* XINE_SARRAY_MODE_DEFAULT */
+  do {
+    int d = sarray->comparator (key, sarray->chunk[m]);
+    if (d == 0)
+      return m; /* found */
+    if (d < 0)
+      e = m;
+    else
+      b = m + 1;
+    m = (b + e) >> 1;
+  } while (b != e);
+  return ~m; /* not found */
 }
