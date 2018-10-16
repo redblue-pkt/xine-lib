68d4688b730e01f2dbff69155dd8c0de5bacf637
Petri Hintukainen <phintuka@users.sourceforge.net>
2018-10-16 09:54:59 +0300

    input_gnome_vfs: ignore ftp/http/test mrls

--- src/input/input_gnome_vfs.c
+++ src/input/input_gnome_vfs.c
@@ -287,7 +287,7 @@ gnomevfs_plugin_open (input_plugin_t *this_gen )
 	return 1;
 }
 
-static const char ignore_scheme[][8] = { "cdda", "file", "http" };
+static const char ignore_scheme[][8] = { "cdda", "file", "ftp", "ftpes", "http", "https", "test", };
 
 static input_plugin_t *
 gnomevfs_klass_get_instance (input_class_t *klass_gen, xine_stream_t *stream,
