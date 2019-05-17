fc25a0bf21b1f4a98d924d10ff26e6631238edb3
Petri Hintukainen <phintuka@users.sourceforge.net>
2019-05-17 11:01:37 +0300

    copy: split _copy_plane()

--- src/xine-utils/copy.c
+++ src/xine-utils/copy.c
@@ -27,41 +27,32 @@
 
 #include <xine/xineutils.h>
 
+static void _copy_plane(uint8_t *restrict dst, const uint8_t *restrict src,
+                        int dst_pitch, int src_pitch,
+                        int width, int height)
+{
+  if (src_pitch == dst_pitch) {
+    xine_fast_memcpy(dst, src, src_pitch * height);
+  } else {
+    int y;
+
+    for (y = 0; y < height; y++) {
+      xine_fast_memcpy(dst, src, width);
+      src += src_pitch;
+      dst += dst_pitch;
+    }
+  }
+}
+
 void yv12_to_yv12
   (const unsigned char *y_src, int y_src_pitch, unsigned char *y_dst, int y_dst_pitch,
    const unsigned char *u_src, int u_src_pitch, unsigned char *u_dst, int u_dst_pitch,
    const unsigned char *v_src, int v_src_pitch, unsigned char *v_dst, int v_dst_pitch,
    int width, int height) {
 
-  int y, half_width = width / 2;
-
-  /* Y Plane */
-  if(y_src_pitch == y_dst_pitch)
-    xine_fast_memcpy(y_dst, y_src, y_src_pitch*height);
-  else {
-    for(y = 0; y < height; y++) {
-      xine_fast_memcpy(y_dst, y_src, width);
-      y_src += y_src_pitch;
-      y_dst += y_dst_pitch;
-    }
-  }
-
-  /* U/V Planes */
-  if((u_src_pitch == u_dst_pitch) && (v_src_pitch == v_dst_pitch)) {
-    xine_fast_memcpy(u_dst, u_src, u_src_pitch*height/2);
-    xine_fast_memcpy(v_dst, v_src, v_src_pitch*height/2);
-  } else {
-    for(y = 0; y < (height / 2); y++) {
-      xine_fast_memcpy(u_dst, u_src, half_width);
-      xine_fast_memcpy(v_dst, v_src, half_width);
-
-      u_src += u_src_pitch;
-      v_src += v_src_pitch;
-
-      u_dst += u_dst_pitch;
-      v_dst += v_dst_pitch;
-    }
-  }
+  _copy_plane(y_dst, y_src, y_dst_pitch, y_src_pitch, width,   height);
+  _copy_plane(u_dst, u_src, u_dst_pitch, u_src_pitch, width/2, height/2);
+  _copy_plane(v_dst, v_src, v_dst_pitch, v_src_pitch, width/2, height/2);
 }
 
 void yuy2_to_yuy2
@@ -69,15 +60,5 @@ void yuy2_to_yuy2
    unsigned char *dst, int dst_pitch,
    int width, int height) {
 
-  int y, double_width = width * 2;
-
-  if(src_pitch == dst_pitch)
-    xine_fast_memcpy(dst, src, src_pitch*height);
-  else {
-    for(y = 0; y < height; y++) {
-      xine_fast_memcpy(dst, src, double_width);
-      src += src_pitch;
-      dst += dst_pitch;
-    }
-  }
+  _copy_plane(dst, src, dst_pitch, src_pitch, width*2, height);
 }
