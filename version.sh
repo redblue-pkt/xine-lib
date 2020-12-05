b5a8a1c1f3f101e97ba29424d8b7b40416b497da
Petri Hintukainen <phintuka@users.sourceforge.net>
2020-12-05 23:34:30 +0200

    Bump version to 1.2.11

--- version.sh
+++ version.sh
@@ -30,14 +30,14 @@
 
 XINE_VERSION_MAJOR=1
 XINE_VERSION_MINOR=2
-XINE_VERSION_SUB=10
+XINE_VERSION_SUB=11
 XINE_VERSION_PATCH=
 # Release series number (usually $XINE_MAJOR.$XINE_MINOR)
 XINE_VERSION_SERIES=1.2
 
-XINE_LT_CURRENT=10
+XINE_LT_CURRENT=11
 XINE_LT_REVISION=0
-XINE_LT_AGE=8
+XINE_LT_AGE=9
 
 if [ -f "`dirname $0`/.cvsversion" ]; then
     HG_REV="`hg summary | sed -e '1s/^parent: \([0-9]*\):.*$/\1/;1q'`"
