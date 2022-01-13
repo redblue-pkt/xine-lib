cd33d6a0e8b084d23ba39e9727934d0b3d794d19
Petri Hintukainen <phintuka@users.sourceforge.net>
2022-01-13 13:08:33 +0200

    Use xine_usec_sleep()

--- src/video_out/video_out_vaapi.c
+++ src/video_out/video_out_vaapi.c
@@ -112,19 +112,6 @@
 #endif
 #endif
 
-#if defined(__linux__)
-// Linux select() changes its timeout parameter upon return to contain
-// the remaining time. Most other unixen leave it unchanged or undefined.
-#define SELECT_SETS_REMAINING
-#elif defined(__FreeBSD__) || defined(__sun__) || (defined(__MACH__) && defined(__APPLE__))
-#define USE_NANOSLEEP
-#elif defined(HAVE_PTHREADS) && defined(sgi)
-// SGI pthreads has a bug when using pthreads+signals+nanosleep,
-// so instead of using nanosleep, wait on a CV which is never signalled.
-#include <pthread.h>
-#define USE_COND_TIMEDWAIT
-#endif
-
 #ifndef HAVE_THREAD_SAFE_X11
 #define LOCK_DISPLAY(_this) XLockDisplay (_this->display)
 #define UNLOCK_DISPLAY(_this) XUnlockDisplay (_this->display)
@@ -454,76 +441,11 @@ typedef struct {
   xine_t              *xine;
 } vaapi_class_t;
 
-static void delay_usec(unsigned int usec)
-{
-    // FIXME: xine_usec_sleep?
-    int was_error;
-
-#if defined(USE_NANOSLEEP)
-    struct timespec elapsed, tv;
-
-    elapsed.tv_sec = 0;
-    elapsed.tv_nsec = usec * 1000;
-
-    do {
-        errno = 0;
-        tv.tv_sec = elapsed.tv_sec;
-        tv.tv_nsec = elapsed.tv_nsec;
-        was_error = nanosleep(&tv, &elapsed);
-    } while (was_error && (errno == EINTR));
-
-#elif defined(USE_COND_TIMEDWAIT)
-    // Use a local mutex and cv, so threads remain independent
-    pthread_cond_t delay_cond = PTHREAD_COND_INITIALIZER;
-    pthread_mutex_t delay_mutex = PTHREAD_MUTEX_INITIALIZER;
-    struct timespec elapsed;
-    uint64_t future;
-
-    future = get_ticks_usec() + usec;
-    elapsed.tv_sec = future / 1000000;
-    elapsed.tv_nsec = (future % 1000000) * 1000;
-
-    do {
-        errno = 0;
-        was_error = pthread_mutex_lock(&delay_mutex);
-        was_error = pthread_cond_timedwait(&delay_cond, &delay_mutex, &elapsed);
-        was_error = pthread_mutex_unlock(&delay_mutex);
-    } while (was_error && (errno == EINTR));
-
-#else // using select()
-    struct timeval tv;
-# ifndef SELECT_SETS_REMAINING
-    uint64_t then, now, elapsed;
-
-    then = get_ticks_usec();
-# endif
-
-    tv.tv_sec = 0;
-    tv.tv_usec = usec;
-
-    do {
-        errno = 0;
-# ifndef SELECT_SETS_REMAINING
-        // Calculate the time interval left (in case of interrupt)
-        now = get_ticks_usec();
-        elapsed = now - then;
-        then = now;
-        if (elapsed >= usec)
-            break;
-        usec -= elapsed;
-        tv.tv_sec = 0;
-        tv.tv_usec = usec;
-# endif
-        was_error = select(0, NULL, NULL, NULL, &tv);
-    } while (was_error && (errno == EINTR));
-#endif
-}
-
 static void vaapi_x11_wait_event(Display *dpy, Window w, int type)
 {
   XEvent e;
   while (!XCheckTypedWindowEvent(dpy, w, type, &e))
-    delay_usec(10);
+    xine_usec_sleep(10);
 }
 
 /* X11 Error handler and error functions */
