d661ffc1f8181ebc8a2c07df8c95d6d9ab479f63
Petri Hintukainen <phintuka@users.sourceforge.net>
2022-02-20 22:29:04 +0200

    Reduce code duplication

--- src/video_out/video_out_opengl2.c
+++ src/video_out/video_out_opengl2.c
@@ -812,72 +812,8 @@ static void opengl2_update_csc_matrix (opengl2_driver_t *that, opengl2_frame_t *
     float saturation = (float)that->saturation / 128.0;
     float contrast = (float)that->contrast / 128.0;
     float brightness = that->brightness;
-    float uvcos = saturation * cos( hue );
-    float uvsin = saturation * sin( hue );
-    int i;
 
-    if ((color_standard >> 1) == 8) {
-      /* YCgCo. This is really quite simple. */
-      uvsin *= contrast;
-      uvcos *= contrast;
-      /* csc_matrix[rgb][yuv1] */
-      that->csc_matrix[1] = -1.0 * uvcos - 1.0 * uvsin;
-      that->csc_matrix[2] =  1.0 * uvcos - 1.0 * uvsin;
-      that->csc_matrix[5] =  1.0 * uvcos;
-      that->csc_matrix[6] =                1.0 * uvsin;
-      that->csc_matrix[9] = -1.0 * uvcos + 1.0 * uvsin;
-      that->csc_matrix[10] = -1.0 * uvcos - 1.0 * uvsin;
-      for (i = 0; i < 12; i += 4) {
-        that->csc_matrix[i] = contrast;
-        that->csc_matrix[i + 3] = (brightness * contrast
-          - 128.0 * (that->csc_matrix[i + 1] + that->csc_matrix[i + 2])) / 255.0;
-      }
-    } else {
-      /* YCbCr */
-      float kb, kr;
-      float vr, vg, ug, ub;
-      float ygain, yoffset;
-
-      switch (color_standard >> 1) {
-        case 1:  kb = 0.0722; kr = 0.2126; break; /* ITU-R 709 */
-        case 4:  kb = 0.1100; kr = 0.3000; break; /* FCC */
-        case 7:  kb = 0.0870; kr = 0.2120; break; /* SMPTE 240 */
-        case 10:
-        case 9:  kb = 0.0593; kr = 0.2627; break; /* BT.2020 */
-        default: kb = 0.1140; kr = 0.2990;        /* ITU-R 601 */
-      }
-      vr = 2.0 * (1.0 - kr);
-      vg = -2.0 * kr * (1.0 - kr) / (1.0 - kb - kr);
-      ug = -2.0 * kb * (1.0 - kb) / (1.0 - kb - kr);
-      ub = 2.0 * (1.0 - kb);
-
-      if (color_standard & 1) {
-        /* fullrange mode */
-        yoffset = brightness;
-        ygain = contrast;
-        uvcos *= contrast * 255.0 / 254.0;
-        uvsin *= contrast * 255.0 / 254.0;
-      } else {
-        /* mpeg range */
-        yoffset = brightness - 16.0;
-        ygain = contrast * 255.0 / 219.0;
-        uvcos *= contrast * 255.0 / 224.0;
-        uvsin *= contrast * 255.0 / 224.0;
-      }
-
-      /* csc_matrix[rgb][yuv1] */
-      that->csc_matrix[1] = -uvsin * vr;
-      that->csc_matrix[2] = uvcos * vr;
-      that->csc_matrix[5] = uvcos * ug - uvsin * vg;
-      that->csc_matrix[6] = uvcos * vg + uvsin * ug;
-      that->csc_matrix[9] = uvcos * ub;
-      that->csc_matrix[10] = uvsin * ub;
-      for (i = 0; i < 12; i += 4) {
-        that->csc_matrix[i] = ygain;
-        that->csc_matrix[i + 3] = (yoffset * ygain
-          - 128.0 * (that->csc_matrix[i + 1] + that->csc_matrix[i + 2])) / 255.0;
-      }
-    }
+    cm_fill_matrix(that->csc_matrix, color_standard, hue, saturation, contrast, brightness);
 
     that->color_standard = color_standard;
     that->update_csc = 0;
